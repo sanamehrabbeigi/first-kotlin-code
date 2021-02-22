c861fa114192faea59a47f341340d4e7ce7e5e4c
sana <sana.mehrab.b@gmail.com>
2021-02-22 22:29:47 +0330

    initial commit

---
+++ src/main/kotlin/ir/sana/controller/mycontroller.kt
@@ -0,0 +1,51 @@
+package ir.sana.controller
+
+import ir.sana.annotation.ApiMapping
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+var translatedWord = ""
+
+val words = setOf( "hello", "cat", "dog", "sun", "blue")
+
+@ApiMapping
+class Mycontroller {
+    @GetMapping("rest")
+    fun get(): ResponseEntity<String> {
+        return ResponseEntity("welcome!", HttpStatus.OK)
+    }
+
+    @PostMapping("rest/{name}")
+    fun post(
+        @PathVariable word: String,
+        @RequestBody language: String
+    ): ResponseEntity<String> {
+        when(language){
+            "chinese" -> { for (input in words) {
+                                when(word) {
+                                    "hello" -> translatedWord = "你好"
+                                    "cat" -> translatedWord = "猫"
+                                    "dog" -> translatedWord = "狗"
+                                    "sun" -> translatedWord = "太阳"
+                                    "blue" -> translatedWord = "蓝色"
+                                    else -> translatedWord = "Not supported!"
+                                }
+                        }
+            }
+            "italian" -> { for (input in words) {
+                                when(word) {
+                                    "hello" -> translatedWord = "Ciao"
+                                    "cat" -> translatedWord = "gatta-gatto"
+                                    "dog" -> translatedWord = "cagna-cane"
+                                    "sun" -> translatedWord = "sole"
+                                    "blue" -> translatedWord = "blu"
+                                    else -> translatedWord = "Not supported!"
+                                }
+                            }
+            }
+            else -> translatedWord = "Not supported!"
+        }
+        return ResponseEntity("means: $translatedWord", HttpStatus.CREATED)
+    }
+}
--- src/main/kotlin/ir/sana/main.kt
+++ src/main/kotlin/ir/sana/main.kt
@@ -1,6 +1,13 @@
 package ir.sana
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+
+@SpringBootApplication
+class myapp
 fun main()
 {
     println("hello sepehr!")
+    SpringApplicationBuilder(myapp::class.java).run()
+
 }
